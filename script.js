4104d9fb23137d7d4c51e25e97ca9a8f2a148fa1
komaudrey <audreyfrebi900@gmail.com>
2025-09-07 22:25:22 +0000

    version3

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function toggleMenu() {
-  const nav = document.querySelector(".nav-links");
-  nav.classList.toggle("active");
+  document.getElementById("nav-links").classList.toggle("active");
 }
-
-// Petit effet au scroll
-window.addEventListener("scroll", () => {
-  document.querySelector("header").classList.toggle("scrolled", window.scrollY > 50);
-});
